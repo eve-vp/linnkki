0a98c22743328757c38ce4a7c279486af8301f44
Evelyn V Pírez <evepirez@gmail.com>
2023-11-07 21:20:49 -0300

    Scroll y Paginación

--- src/components/MovieTable.tsx
+++ src/components/MovieTable.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const API_KEY = '480128c3202788f17d08d104b8f5c03c';
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
 
 const MovieTable = () => {
   const [movies, setMovies] = useState([]);
@@ -26,51 +26,68 @@ const MovieTable = () => {
     setPage(page + 1);
   };
 
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Poster</th>
-            <th>Title</th>
-            <th>Release Year</th>
-          </tr>
-        </thead>
-        <tbody>
-        {movies.map(movie => (
-        <tr key={movie.id}>
-        <td>
-          <img 
-            className="movie-poster"
-            src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
-            alt={movie.title} 
-          />
-        </td>
-        <td>{movie.title}</td>
-        <td>{movie.release_date.substring(0, 4)}</td>
-        </tr> 
-
-      ))}
-      </tbody>
-
-        </table>
-          <button onClick={handlePrevPage} disabled={page === 1}>
-          Previous Page
-          </button>
-          <button onClick={handleNextPage}>Next Page</button>
+  const itemsPerRow = 4; // Número de películas por fila
+  const totalRows = Math.ceil(movies.length / itemsPerRow); // Total de filas
+  const itemsPerPage = itemsPerRow * 2; // Número de películas por página
+  const totalPages = Math.ceil(movies.length / itemsPerPage); // Total de páginas
+  
+  const renderMovies = (start, end) => {
+    return movies.slice(start, end).map((movie) => (
+      <div key={movie.id} style={{ margin: '10px', textAlign: 'center', flex: '0 0 25%' }}>
+        <img
+          className="movie-poster"
+          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div style={{ marginTop: '10px' }}>
+          <p style={{ marginBottom: '5px' }}>{movie.title}</p>
+          <p>{movie.release_date.substring(0, 4)}</p>
+        </div>
       </div>
+    ));
+  };
+  
+  return (
+    <div style={{ maxWidth: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', overflow: 'hidden' }}>
+      <div style={{ maxHeight: 'calc(100vh - 200px)', overflowY: 'scroll', width: '100%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px' }}>
+        {renderMovies(0, movies.length)}
+      </div>
+      <div style={{ marginTop: '20px', marginBottom: '20px', maxWidth: '800px', display: 'flex', justifyContent: 'center' }}>
+        <button onClick={handlePrevPage} disabled={page === 1}>
+          Previous Page
+        </button>
+        {Array.from({ length: Math.min(totalPages, 10) }, (_, index) => (
+          <button key={index + 1} onClick={() => setPage(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+        <button onClick={handleNextPage} disabled={page === totalPages}>
+          Next Page
+        </button>
+      </div>
+    </div>
   );
+  
 };
 
-const getMovies = async (page: number) => {
-  const response = await axios.get(`${BASE_URL}/discover/movie`, {
-    params: {
-      api_key: API_KEY,
-      page: page,
-    },
-  });
+const getMovies = async (page) => {
+  const params = {
+    api_key: API_KEY,
+    page: page,
+    language: 'en-US',
+    sort_by: 'popularity.desc',
+    include_adult: false,
+    include_video: false,
+  };
 
-  return response.data;
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    // Manejo de errores
+    console.error('Error fetching movies:', error);
+    return [];
+  }
 };
 
 export default MovieTable;
