0f94e35bb759bed98e11e5db7098dca9904886a8
Evelyn V Pírez <evepirez@gmail.com>
2023-11-28 20:41:39 -0300

    Routes Actualización

--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,44 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   BrowserRouter, 
   Routes, 
   Route 
 } from 'react-router-dom';
-import { createRoot } from 'react-dom/client'; 
 import App from './App';
 import MovieGrid from './components/MovieGrid';
-import './index.css';
 import Pagination from './components/Pagination';
 import SearchMovie from './components/SearchMovie';
+import OrderMovie from './components/OrderMovie';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error('Failed to find the root element');
 }
 
-const root = createRoot(rootElement);
-
-// const routing = (
-  root.render(
-<React.StrictMode>
-<BrowserRouter>
-  <Routes>
-
-    <Route path="/" element={<App />} />
-    <Route path="Movies" element={<MovieGrid />} />
-    <Route path="Pagination" element={<Pagination currentPage={0} totalPages={0} onPageChange={function (): void {
-            throw new Error('Function not implemented.');
-          } } />} />
-    <Route path="SearchMovie" element={<SearchMovie onSearch={function (): void {
-            throw new Error('Function not implemented.');
-          } } />} />
-
-  </Routes> 
-</BrowserRouter>
-</React.StrictMode>
-
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="Movies" element={<MovieGrid />} />
+        <Route
+          path="Pagination"
+          element={<Pagination currentPage={0} totalPages={0} onPageChange={() => {}} />}
+        />
+        <Route
+          path="SearchMovie"
+          element={<SearchMovie onSearch={() => {}} />}
+        />
+        <Route
+          path="OrderMovie"
+          element={<OrderMovie currentOrder="" onOrderChange={() => {}} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
 );
+
 
 // "*" wildcard route to match any path
